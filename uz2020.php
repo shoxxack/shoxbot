c4ccfdcaf9598db82bf4d275685474e57effeb7b
shoxxack <73601807+shoxxack@users.noreply.github.com>
2020-11-04 14:20:25 +0500

    Add files via upload

---
+++ uz2020.php
@@ -0,0 +1,113 @@
+<?php
+ob_start();
+define('API_KEY','1477002303:AAEyf3bE19xp7DcZESVlArvi91gjyJhrIlc');
+$admin = '1232898350';
+
+function bot($method,$datas=[]){
+$url = "https://api.telegram.org/bot".API_KEY."/$method";
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+$res = curl_exec($ch);
+if(curl_error($ch)){
+var_dump(curl_error($ch));
+}else{
+return json_decode($res);
+}
+}
+
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$mid = $message->message_id;
+$data = $update->callback_query->data;
+$type = $message->chat->type;
+$text = $message->text;
+$cid = $message->chat->id;
+$uid= $message->from->id;
+$message = $update->message;
+$cid = $message->chat->id;
+$cidtyp = $message->chat->type;
+$miid = $message->message_id;
+$name = $message->chat->first_name;
+$user1 = $message->from->username;
+$tx = $message->text;
+$callback = $update->callback_query;
+$mmid = $callback->inline_message_id;
+$mes = $callback->message;
+$mid = $mes->message_id;
+$cmtx = $mes->text;
+$mmid = $callback->inline_message_id;
+$idd = $callback->message->chat->id;
+$cbid = $callback->from->id;
+$cbuser = $callback->from->username;
+$data = $callback->data;
+$ida = $callback->id;
+$cqid = $update->callback_query->id;
+$cbins = $callback->chat_instance;
+$cbchtyp = $callback->message->chat->type;
+$step = file_get_contents("step/$cid.step");
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$mid = $message->message_id;
+$msgs = json_decode(file_get_contents('msgs.json'),true);
+$data = $update->callback_query->data;
+$type = $message->chat->type;
+$text = $message->text;
+$cid = $message->chat->id;
+$uid= $message->from->id;
+$gname = $message->chat->title;
+$left = $message->left_chat_member;
+$new = $message->new_chat_member;
+$name = $message->from->first_name;
+$bio = $messenge->from->about;
+$repid = $message->reply_to_message->from->id;
+$repname = $message->reply_to_message->from->first_name;
+$newid = $message->new_chat_member->id;
+$leftid = $message->left_chat_member->id;
+$newname = $message->new_chat_member->first_name;
+$leftname = $message->left_chat_member->first_name;
+$username = $message->from->username;
+$cmid = $update->callback_query->message->message_id;
+$cusername = $message->chat->username;
+$repmid = $message->reply_to_message->message_id; 
+$ccid = $update->callback_query->message->chat->id;
+$cuid = $update->callback_query->message->from->id;
+
+$key = json_encode([
+'resize_keyboard'=>true,
+'keyboard'=>[
+[['text'=>"⚡Bot Tezligi⚡"]],
+]
+]);
+
+if($text == "/start"){
+	bot('sendmessage',[
+	'chat_id'=>$cid,
+	'text'=>"*Shox Xacker botlar tezligini oʻlchash uchun yaratdi!*",
+	'parse_mode'=>"markdown",
+	'reply_markup'=>$key,
+]);
+}
+
+if($text == "⚡Bot Tezligi⚡"){
+$start_time = round(microtime(true) * 1000);
+      $send=  bot('sendmessage', [
+                'chat_id' => $cid,
+                'text' =>"*@Tezlik_Sbot Tezligi:*",
+                        'parse_mode'=>"markdown",
+            ])->result->message_id;
+        
+                    $end_time = round(microtime(true) * 1000);
+                    $time_taken = $end_time - $start_time;
+                    bot('editMessagetext',[
+                        "chat_id" => $cid,
+                        "message_id" => $send,
+                        "text" => "*Botimiz Tezligi:" . $time_taken .  "Ms*",
+                        'parse_mode'=>"markdown",
+                    ]);
+}
+
+/*Tezlik Bot kodi*/
+
+?>
